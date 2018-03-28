21c12e1a2999ff21c3c7d041a741113ca44457c3
Tyler Jankowski <35941543+TJank@users.noreply.github.com>
2018-03-28 16:53:37 -0500

    Work on webapp

--- webapp/app.js
+++ webapp/app.js
@@ -1,3 +1,5 @@
+import { mongo } from 'mongoose';
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,6 +10,16 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://tjdbuser:tjdbpass@ds037185.mlab.com:37185/local_library_tj';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
